4d9440dba0909a4fb77e505f61fbc663a064d259
KLSchramke <110642686+KLSchramke@users.noreply.github.com>
2024-08-17 19:01:29 -0500

    Update Sun_Earth_Moon Trajectories.py

--- Sun_Earth_Moon Trajectories.py
+++ Sun_Earth_Moon Trajectories.py
@@ -1,6 +1,6 @@
 # This is to be the 3D animated trajectory of the Earth as it orbits the Sun, and the Moon as it orbits the Earth around the Sun.
 # This is intended to add the trajectory of the Moon to the Earth-Sun system as programmed in the file
-# titled Earth-Sun.py
+# titled Earth_Sun.py
 
 import numpy as np
 import matplotlib.pyplot as plt
