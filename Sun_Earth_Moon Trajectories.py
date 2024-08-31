73ecf1daff21c00c34b771006fe977e10319f79c
KLSchramke <110642686+KLSchramke@users.noreply.github.com>
2024-08-31 17:11:24 -0500

    Update Sun_Earth_Moon Trajectories.py
    corrections still needed. Moon trajectory not working right yet

--- Sun_Earth_Moon Trajectories.py
+++ Sun_Earth_Moon Trajectories.py
@@ -1,4 +1,4 @@
-# This creates a 3D animated trajectory of the Earth as it orbits the Sun, and the Moon as it orbits the Earth around the Sun.
+# This will be a 3D animated trajectory of the Earth as it orbits the Sun, and the Moon as it orbits the Earth around the Sun.
 # This is intended to add the trajectory of the Moon to the Earth-Sun system as programmed in the file
 # titled Earth_Sun.py
 
